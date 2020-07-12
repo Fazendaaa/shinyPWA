74c0d3bac29b08e7f32542e78f5ece5351192ef6
Fazendaaa <lucas.carotta@outlook.com>
2020-07-12 15:59:13 -0300

    Adding jall to cache

--- inst/js/serviceWorker.js
+++ inst/js/serviceWorker.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', installEvent => {
     installEvent.waitUntil(
         caches.open(websiteVersion).then(cache => {
-            cache.addAll([ '/' ]);
+            cache.addAll([ '/*' ]);
         })
     )
 })
